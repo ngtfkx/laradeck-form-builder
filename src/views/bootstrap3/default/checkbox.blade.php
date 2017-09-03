2140c603ea56074b8cb03b0f6db257fb18432280
Denis Sandal <den.sandal@gmail.com>
2017-09-03 18:01:49 +0700

    Fix b3 default checkbox tamplate

--- src/views/bootstrap3/default/checkbox.blade.php
+++ src/views/bootstrap3/default/checkbox.blade.php
@@ -1,14 +1,15 @@
 <div class="form-group{{ $errors->has($fieldName) ? ' has-error' : '' }}">
-    <label for="{{ $id }}" class="col-sm-2 control-label">{{ $label }}</label>
-    <div class="col-sm-10">
-        {!! $tag  !!}
-        @if ($errors->has($fieldName))
-            <span class="help-block">
+    <div class="checkbox">
+        <label>
+            {!! $tag  !!} {{ $label }}
+        </label>
+    </div>
+    @if ($errors->has($fieldName))
+        <span class="help-block">
                 <strong>{{ $errors->first($fieldName) }}</strong>
             </span>
-        @endif
-        @if(!is_null($help) && !empty($help))
-            <p class="help-block">{{ $help }}</p>
-        @endif
-    </div>
+    @endif
+    @if(!is_null($help) && !empty($help))
+        <p class="help-block">{{ $help }}</p>
+    @endif
 </div>
